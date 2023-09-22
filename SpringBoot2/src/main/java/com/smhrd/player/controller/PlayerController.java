21f6f54eb46aa3cfcf9e3f5ac736c0e7b58df61b
lim-mil <lakm1225@gmail.com>
2023-09-22 14:22:33 +0900

    ㅎㅎ

--- SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
+++ SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smhrd.player.service.PlayerService;
 
 @RestController // 리액트 서버로 데이터만 응답
-@CrossOrigin("http://172.31.47.89:3001/") // aws : 서버주소 변경
+@CrossOrigin("http://43.200.176.140:3001/") // aws : 서버주소 변경
+
+
 public class PlayerController {
 	
 	@Autowired
